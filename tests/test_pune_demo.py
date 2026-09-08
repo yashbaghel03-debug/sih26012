ae2623b5b2a00b96c8acf7d88017c52ca0ddd226
yashbaghel03-debug <yashbhagel03@gmail.com>
2026-09-09 00:21:42 +0530

    test: tighten Pune demo determinism checks

--- tests/test_pune_demo.py
+++ tests/test_pune_demo.py
@@ -7,13 +7,14 @@ def test_pune_seed_is_deterministic_and_24_records():
     second = list(parcel_seed())
     assert len(first) == 24
     assert first == second
-    assert all(x['geometry'] if 'geometry' in x else True for x in [])
-    assert all(x['geometry_status'].startswith('N/D') for x in first)
-    assert all(x['ulpin'] is None for x in first)
+    assert all(x["geometry_status"].startswith("N/D") for x in first)
+    assert all(x["ulpin"] is None for x in first)
+    assert first[0]["parcel_id"] == "PUNE-KOT-DEMO-001"
+    assert first[-1]["parcel_id"] == "PUNE-KOT-DEMO-024"
 
 
 def test_existing_alu_remains_deterministic():
-    a = Cell('1km2', 12, 34, (56,))
-    b = Cell('1km2', 12, 34, (56,))
+    a = Cell("1km2", 12, 34, (56,))
+    b = Cell("1km2", 12, 34, (56,))
     assert a.id == b.id
     assert a.area_m2 == 1_000_000.0
